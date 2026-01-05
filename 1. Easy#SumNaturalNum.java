90e0ce929906419b9937449af56e14a625c5a8b5
Vanshika Jaiswal <itsvanil64@gmail.com>
2026-01-05 10:49:12 +0530

    Implement sum Of n Natural numbers function

---
+++ 1. Easy#SumNaturalNum.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+
+class Solution {
+    static int sumOfNaturals(int n) {
+        // code here
+        int sum= n*(n+1)/2;
+        return sum;
+        
+    }
+};
