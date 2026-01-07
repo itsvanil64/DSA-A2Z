1b4c3382da7137d9dc88c925e59f84f4824c6f4d
Vanshika Jaiswal <itsvanil64@gmail.com>
2026-01-07 23:49:42 +0530

    Refactor palindrome check for clarity

--- 4. Easy#PalindromeNum.java
+++ 4. Easy#PalindromeNum.java
@@ -3,14 +3,14 @@ public:
     bool isPalindrome(int x) {
         int temp=x;
         long revNum=0;
+        if(x<0) 
+            return false;
         while(x>0){
             int digit=x%10;
             revNum= revNum * 10 + digit;
             x=x/10;
         }
-        if(revNum == temp ){
-            return true;
-        }
-        return false;
+      
+        return revNum==temp;
     }
 };
