0cc84e7004b198ae9c397db04c85e01301700883
Vanshika Jaiswal <itsvanil64@gmail.com>
2026-01-10 16:22:22 +0530

    return true or false for a leap year

---
+++ 6. Easy#LeapYear.java
@@ -0,0 +1,11 @@
+class Solution {
+    static boolean isLeap(int N) {
+        // code here
+    
+        if(N%400==0)
+        return true;
+        else if(N%4==0 && N%100!=0)
+        return true;
+        else return false;
+    }
+}
